d04fe6c649b6d7d8db936104e154e70238179de3
th11jr@aol.com <th11jr@aol.com>
2024-01-26 21:23:45 -0600

    Added better algo for palindrome problem. Moving on to ZigZag

--- largest_palindromic_substring/src/main.rs
+++ largest_palindromic_substring/src/main.rs
@@ -1,45 +1,51 @@
-use std::collections::HashMap;
-
 struct Solution{}
 
 // TODO: Implement better solution. I could just take each character and attempt to expand to each side to find longest palindrome.
 impl Solution {
-    fn is_palindrome(s: &str) -> bool {
-        let chars: Vec<char> = s.chars().collect();
-        for (i, &ch) in chars.iter().enumerate() {
-            if  ch != chars[chars.len() - 1 - i] { return false; }
-            if i >= chars.len()/2 { break; }
-        }
-
-        true
-    }
-
     pub fn largest_palindrome(s: String) -> String {
-        let mut largest_str: String = String::new();
-        let mut char_pos: HashMap<char, Vec<usize>> = HashMap::new();
-
-        for (i, ch) in s.chars().enumerate() {
-            // get the positions this character has been in before
-            let positions = char_pos.entry(ch).or_insert(Vec::new());
-            positions.push(i);
-
-            // check all string slices between those previous characters to see if they are palindromes
-            for &pos in positions.as_slice() {
-                let str_len = i-pos+1;
-                if str_len > largest_str.len() {
-                    if Self::is_palindrome(&s[pos..=i]) {
-                        largest_str = s[pos..=i].to_owned();
-                    }
+        let str: Vec<char> = s.chars().collect();
+        let n = str.len();
+        let mut l_pal: String = String::new();
+        
+        for i in 0..n {
+            let (mut left, mut right) = (i, i);
+            loop {
+                match (str.get(left), str.get(right)) {
+                    (Some(lch), Some(rch)) => {
+                        if lch == rch {
+                            if right - left + 1 > l_pal.len() {
+                                l_pal = str[left..=right].iter().collect();
+                            }
+                        } else { break; }
+                    },
+                    _ => (),
                 }
+                if left == 0 || right == n-1 { break ;}
+                left -= 1;
+                right += 1;
             }
-//            positions.push(i);
+        
+            let (mut left, mut right) = (i, i+1);
+            loop {
+                match (str.get(left), str.get(right)) {
+                    (Some(lch), Some(rch)) => {
+                        if lch == rch {
+                            if right - left + 1 > l_pal.len() {
+                                l_pal = str[left..=right].iter().collect();
+                            }
+                        } else { break; }
+                    },
+                    _ => (),
+                }
+                if left == 0 || right == n-1 { break ;}
+                left -= 1;
+                right += 1;
+            }
         }
-    
-        largest_str
+        
+        l_pal
     }
-
 }
-
 
 fn main() {
     let test_string = String::from("babad");
