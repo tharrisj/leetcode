6d31c9d78318d1912799b89fa7bff3ef83aaa8dd
th11jr@aol.com <th11jr@aol.com>
2024-01-27 08:48:44 -0600

    Added solution to zigzag. can probably improve

--- zigzag_conversion/src/main.rs
+++ zigzag_conversion/src/main.rs
@@ -2,7 +2,38 @@ struct Solution{}
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows == 1 {
+            return s;
+        }
         let mut zz_ret = String::new();
+        let mut filler: Vec<Vec<char>> = vec!(vec!(' ';s.len());num_rows as usize);
+
+        let mut v_mov: i32 = 1;
+        let mut h_mov: i32 = 0;
+        let mut v_ind: i32 = 0;
+        let mut h_ind: i32 = 0;
+        for ch in s.chars() {
+            filler[v_ind as usize][h_ind as usize] = ch;
+
+            if v_ind == num_rows - 1 {
+                v_mov = -1;
+                h_mov = 1;
+            } else if v_ind == 0 {
+                v_mov = 1;
+                h_mov = 0;
+            }
+
+            v_ind += v_mov;
+            h_ind += h_mov;
+        }
+
+        for c_vec in filler {
+            let tmp: String = c_vec
+                .iter()
+                .filter(|&&ch| ch != ' ')
+                .collect();
+            zz_ret.push_str(&tmp);
+        }
 
         zz_ret
     }
