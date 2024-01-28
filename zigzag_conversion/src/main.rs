1bfe5d8cd737276cb81bfd276f8d4009a9d23256
th11jr@aol.com <th11jr@aol.com>
2024-01-28 17:35:01 -0600

    This is how to implement the reverse_integer very easily

--- zigzag_conversion/src/main.rs
+++ zigzag_conversion/src/main.rs
@@ -2,29 +2,24 @@ struct Solution{}
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        if num_rows == 1 {
-            return s;
-        }
+        if num_rows == 1 { return s; }
         let mut zz_ret = String::new();
         let mut filler: Vec<Vec<char>> = vec!(vec!(' ';s.len());num_rows as usize);
 
-        let mut v_mov: i32 = 1;
-        let mut h_mov: i32 = 0;
+        let mut rev: bool = false;
         let mut v_ind: i32 = 0;
         let mut h_ind: i32 = 0;
         for ch in s.chars() {
             filler[v_ind as usize][h_ind as usize] = ch;
 
-            if v_ind == num_rows - 1 {
-                v_mov = -1;
-                h_mov = 1;
-            } else if v_ind == 0 {
-                v_mov = 1;
-                h_mov = 0;
-            }
+            if v_ind == 0 || v_ind == num_rows-1 { rev = !rev; }
 
-            v_ind += v_mov;
-            h_ind += h_mov;
+            if rev {
+                v_ind += 1;
+            } else {
+                v_ind -= 1;
+                h_ind += 1;
+            }
         }
 
         for c_vec in filler {
