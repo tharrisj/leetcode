376e589be3ed9d4c6654ef6acac2e7d95aeaf0d4
thomas <th11jr@aol.com>
2024-03-03 19:29:35 -0600

    Daily Question

--- questions/src/bin/19.rs
+++ questions/src/bin/19.rs
@@ -33,7 +33,11 @@ struct Solution;
 
 impl Solution{
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-        todo!()
+        let cnt = std::iter::successors(head.as_ref(), |last| last.next.as_ref()).count();
+        let mut dummy = Some(Box::new(ListNode{val: 0, next: head}));
+        let mut prev = (0..cnt - n as usize).fold(dummy.as_mut(), |curr, _| curr.unwrap().next.as_mut() );
+        prev.unwrap().next = prev.as_mut().unwrap().next.as_mut().unwrap().next.take();
+        dummy.unwrap().next
     }
 }
 
